44db3bdc3a0f478e47c97b6cdb79a291589c662e
JogaSingh <joga.singh-extern@deutschebahn.com>
2021-01-30 16:14:36 +0100

    war build

--- comp-mgmt/src/main/java/net/devoat/component/ComponentManagerApplication.java
+++ comp-mgmt/src/main/java/net/devoat/component/ComponentManagerApplication.java
@@ -6,13 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "net")
 @EnableJpaRepositories("net")
 @EntityScan(basePackages= "net")
-public class ComponentManagerApplication extends WebSecurityConfigurerAdapter {
+public class ComponentManagerApplication {
 	private static ApplicationContext ctx;
 	public static void main(String[] args) {
 		ctx = SpringApplication.run(ComponentManagerApplication.class, args);
